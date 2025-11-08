4e73011d670643da24be8b860310e0f4ff54dbe5
Abel García Nájera <abel.garcia.n@gmail.com>
2025-11-08 17:24:29 -0600

    Rename enum Transacción to Transaccion

--- simulador.cpp
+++ simulador.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-enum Transacción { COMPRA, VENTA };
+enum Transaccion { COMPRA, VENTA };
 
 
 void comprar(SistemaCV &s, int dia, int tid, int cantidad, int costo) {
@@ -46,7 +46,7 @@ void mostrarEstado(SistemaCV &s) {
 
 int main() {
     int D_MAX = 100;
-    Transacción transacción[] = {COMPRA, COMPRA, COMPRA, VENTA, COMPRA, VENTA, COMPRA, COMPRA, VENTA, VENTA, COMPRA, VENTA};
+    Transaccion transaccion[] = {COMPRA, COMPRA, COMPRA, VENTA, COMPRA, VENTA, COMPRA, COMPRA, VENTA, VENTA, COMPRA, VENTA};
     int dias[] = {3, 10, 15, 20, 48, 50, 66, 73, 82, 88, 92, 99};
     int costo[] = {30, 30, 40, 50, 40, 60, 50, 40, 50, 60, 60, 50};
     int cantidad[] = {20, 40, 200, 100, 100, 200, 50, 200, 200, 100, 40, 50};
@@ -56,13 +56,13 @@ int main() {
     int trans = 1;
     for (int dia = 1; dia <= D_MAX; dia++) {
         if (dia == dias[d]) {
-            if (transacción[d] == COMPRA)
+            if (transaccion[d] == COMPRA)
                 comprar(s, dia, trans, cantidad[d], costo[d]);
             else
                 vender(s, dia, trans, cantidad[d], costo[d]);
             mostrarEstado(s);
-            trans += 1;
-            d += 1;
+            trans++;
+            d++;
         }
     }
     cout << "Fin del período." << endl;
